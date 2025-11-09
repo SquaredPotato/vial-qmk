7a696e82b27c67549eea2a9eef5bf8e99fe21b6e
stefan <20231417+SquaredPotato@users.noreply.github.com>
2025-11-09 21:28:25 +0100

    feat: bongocat

--- keyboards/splitkb/kyria/keymaps/default/rules.mk
+++ keyboards/splitkb/kyria/keymaps/default/rules.mk
@@ -7,5 +7,7 @@ VIAL_ENABLE = yes
 VIALRGB_ENABLE = yes
 QMK_SETTINGS = yes
 
+WPM_ENABLE = yes
+
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 ENCODER_MAP_ENABLE = yes   # Map encoders using Vial
