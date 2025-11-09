520a60715a11ae6d86ba9fc4c32835d15d711d3e
stefan <20231417+SquaredPotato@users.noreply.github.com>
2025-11-09 17:53:20 +0100

    feat: add initial kyria v1 overridable vial keymap

--- keyboards/splitkb/kyria/keymaps/default/rules.mk
+++ keyboards/splitkb/kyria/keymaps/default/rules.mk
@@ -1,4 +1,13 @@
 OLED_ENABLE = yes
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGB_MATRIX_ENABLE = yes     # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIALRGB_ENABLE = yes
+QMK_SETTINGS = yes
+
+
+
+
+ENCODER_MAP_ENABLE = no
